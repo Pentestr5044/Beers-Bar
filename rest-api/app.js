e8874f66af7880d0e907307ee5c3fb6f147d40f3
Pentestr5044 <pentestr5044@gmail.com>
2022-07-21 18:32:16 -0700

    setting up the rest of the boilerplate
    BOILERPLATE FOR THE STARTING OF THE PROJECT AY!

---
+++ rest-api/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express()
+const port = 8888
+
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log("connected on port 8888.....running!")
+})
+
