52d9e11538f8dedd5b649c9e9be2c9d2ca996eca
Pentestr5044 <pentestr5044@gmail.com>
2022-07-27 16:45:03 -0700

    started routes for front-end
    routes for frontend pages are done and I can start making the pages as exports for the router. backend env files are made for express and api easy calls.

--- frontend/beers-bar-frontend/src/index.js
+++ frontend/beers-bar-frontend/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/creators" element={<Creator />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Register />} />
+      <Route path="/profile-update" element={<UpdateProfile />} />
+      <Route path="/staff" element={<Staff />} />
+      <Route path="/private-chats" element={<Private />} />
+      <Route path="/premium" element={<Premium />} />
+      <Route path="/my/profile" element={<UserProfile />} />
+      <Route path="/mods" element={<Moderators />} />
+      <Route path="/admin/meeting" element={<MeetingTable />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="/chatrooms" element={<Chatrooms />} />
+      <Route path="/buy-a-gift" element={<BuyABeer />} />
+      <Route path="/my/bio" element={<Bio />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
