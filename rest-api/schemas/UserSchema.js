332cd29c689ced91c92c5beb5dec572ef7cd249b
Pentestr5044 <pentestr5044@gmail.com>
2022-08-03 13:50:34 -0700

    created user schema for database in mongodb
    database for backend

---
+++ rest-api/schemas/UserSchema.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema ({
+    username:{ 
+        type: String, 
+        req: true,
+        unique: true
+    },
+    email:{ 
+        type: email,
+        unique: true, 
+        req: true
+    },
+    password:{ 
+        type: String, 
+        req: true
+    },
+    fName:{ 
+        type: String, 
+        req: true
+    },
+    lName:{ 
+        type: String, 
+        req: true
+    },
+    rDate:{ 
+        type: Date, 
+        req: true,
+        hidden: true
+    }
+})
+module.exports = mongoose.model('users', userSchema);
