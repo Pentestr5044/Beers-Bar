08af230133606356ed19afcc417ff7956c322897
Pentestr5044 <pentestr5044@gmail.com>
2022-08-17 19:24:44 -0700

    frontend started
    Started mapping out front end and started components for front end.

---
+++ frontend/beers-bar-frontend/src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route,} from 'react-router-dom';
+import Home from './pages/home/Home'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
+ //<Route path="/home" element={<Home />} />
+      //<Route path="/about" element={<About />} />
+      //<Route path="/creators" element={<Creator />} />
+      //<Route path="/login" element={<Login />} />
+      //<Route path="/registration" element={<Register />} />
+      //<Route path="/profile-update" element={<UpdateProfile />} />
+      //<Route path="/staff" element={<Staff />} />
+      //<Route path="/private-chats" element={<Private />} />
+      //<Route path="/premium" element={<Premium />} />
+      //<Route path="/my/profile" element={<UserProfile />} />
+      //<Route path="/mods" element={<Moderators />} />
+      //<Route path="/admin/meeting" element={<MeetingTable />} />\
+      //<Route path="/logout" element={<Logout />} />
+      //<Route path="/chatrooms" element={<Chatrooms />} />
+      //<Route path="/buy-a-gift" element={<BuyABeer />} />
+      //<Route path="/my/bio" element={<Bio />} />
+      //<Route path="/admin" element={<Admin />} />
+      //<Route path="/about" element={<About />} />
+
