64b07264dfc92c0d8582757b8b2553b36257524e
Pentestr5044 <pentestr5044@gmail.com>
2022-08-19 20:23:13 -0700

    made some changes to the topbar
    Making the css for the topbar

--- frontend/beers-bar-frontend/src/components/Topbar.jsx
+++ frontend/beers-bar-frontend/src/components/Topbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './topbar.css'
-import testImage from '../assets/icons/wallet2.svg'
+import testImage from '../assets/icons/currency-dollar.svg'
 export default function Topbar() {
   return (
     <div>
         <span className="top-container">
             <div className="left-bar">
-               <label labelFor="wallet">wallet
-               <img className="img-test" id="wallet" src={testImage} alt="null"/>
-               </label>
-               
+                <button className="topbar-btn-menu-left" title="Buy a Beer"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
             </div>
-            <div className="right-bar">right</div>
+            <div className="right-bar">              
+                <button className="topbar-btn-menu" title="Login"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Logout"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Gift A Beer"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Messages"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Profile"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="About Our Beer"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Creator"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+                <button className="topbar-btn-menu" title="Admin"><img className="img-test" id="wallet" src={testImage} alt="null" altText="buy a beer"/></button>
+              </div>
         </span>
     </div>
   )
