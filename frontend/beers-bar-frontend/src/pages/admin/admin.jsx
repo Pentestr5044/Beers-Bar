9fb66760a0b2daa4425b3d0eda8c8a3794f9c7ed
Pentestr5044 <pentestr5044@gmail.com>
2022-08-27 17:12:43 -0700

    admin panel done
    admin panel is done and we are moving on to the rest of the page designs. after all of the pages front end are designed we will slowly start to use Axiom to run our data for us.

--- frontend/beers-bar-frontend/src/pages/admin/admin.jsx
+++ frontend/beers-bar-frontend/src/pages/admin/admin.jsx
@@ -1,4 +1,5 @@
 import './admin.css'
+import AdminImg from '../../assets/tavern/tavern-door.jpg'
 import Topbar from '../../components/Topbar'
 
 
@@ -8,13 +9,27 @@ export default function Admin() {
         <><Topbar/></>
         <span className="admin-cont">
             <span className="admin-left">
-                <h1>Admin Left</h1>
+                <h1>Admin User Panel</h1>
+                <ul>
+                    <li className="list-item"><button className="edit-user">Edit User</button></li>
+                    <li className="list-item"><button className="edit-user">Create User</button></li>
+                    <li className="list-item"><button className="edit-user">Delete User</button></li>
+                    <li className="list-item"><button className="edit-user">View User</button></li>
+                </ul>
             </span>
             <span className="admin-middle">
-                <h1>Admin picture and welcome message</h1>
+                <h1>Welcome Admin!</h1>
+                <p>This is the section where you can review your messages or even respond to messages. Also you can add beer to our inventory from any producer you want.</p>
+                <img src={AdminImg} alt="" className="admin-img" />
             </span>
-            <span className="admin-right">
-                <h1>beer posting</h1>
+            <span className="admin-left">
+                <h1>BEER EDITS</h1>
+                <ul>
+                    <li className="list-item"><button className="edit-user">Edit Beer</button></li>
+                    <li className="list-item"><button className="edit-user">Create Beer</button></li>
+                    <li className="list-item"><button className="edit-user">Delete Beer</button></li>
+                    <li className="list-item"><button className="edit-user">View Beer</button></li>
+                </ul>
             </span>
 
         </span>
